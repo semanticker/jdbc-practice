3c311d3f631f636c6d95a278ada0d9d147273a80
semanticker <semanticker@gmail.com>
2023-03-14 07:53:09 +0900

    User 저장, 조회 테스트 추가

---
+++ src/main/java/org/example/UserDao.java
@@ -0,0 +1,91 @@
+package org.example;
+
+import java.sql.*;
+
+public class UserDao {
+    public void create(User user) throws SQLException {
+
+        Connection con = null;
+        PreparedStatement pstmt = null;
+
+        try {
+            con = getConnection();
+            String sql = "INSERT INTO USERS values (?, ?, ?, ?)";
+
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, user.getUserId());
+            pstmt.setString(2, user.getPassword());
+            pstmt.setString(3, user.getName());
+            pstmt.setString(4, user.getEmail());
+
+            pstmt.executeUpdate();
+
+        } finally {
+            if (pstmt != null) {
+                pstmt.close();
+            }
+
+            if (con !=null) {
+                con.close();
+            }
+        }
+
+    }
+
+    private Connection getConnection() {
+        String URL = "jdbc:h2:mem://localhost/~/jdbc-practice;MODE=MySQL;DB_CLOSE_DELAY=1";
+        String ID = "sa";
+        String PW = "";
+
+        try {
+            Class.forName("org.h2.Driver");
+            return DriverManager.getConnection(URL, ID, PW);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public User findByUserId(String userId) throws SQLException {
+
+
+        Connection con = null;
+
+        PreparedStatement pstmt = null;
+
+        ResultSet rs = null;
+
+        try {
+            con = getConnection();
+
+            String sql = "SELECT userId, password, name, email FROM USERS WHERE userid = ?";
+            con.prepareStatement(sql);
+            pstmt.setString(1, userId);
+
+            rs = pstmt.executeQuery();
+
+            User user = null;
+            if (rs.next()) {
+                user = new User(
+                        rs.getString("userId"),
+                        rs.getString("password"),
+                        rs.getString("name"),
+                        rs.getString("email")
+                );
+            }
+
+            return user;
+
+        } finally {
+            if (pstmt != null) {
+                pstmt.close();
+            }
+
+            if (con !=null) {
+                con.close();
+            }
+        }
+
+    }
+}
--- src/test/java/org/example/UserDaoTest.java
+++ src/test/java/org/example/UserDaoTest.java
@@ -1,9 +1,14 @@
 package org.example;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
 import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+
+import java.sql.SQLException;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class UserDaoTest {
 
@@ -13,5 +18,15 @@ public class UserDaoTest {
         populator.addScript(new ClassPathResource("db_schema.sql"));
         //
         DatabasePopulatorUtils.execute(populator, ConnectionManager.getDataSource());
+    }
+
+    @Test
+    void createTest() throws SQLException {
+        String USER_ID = "";
+        UserDao userDao = new UserDao();
+        userDao.create(new User(USER_ID,"password","name", "email"));
+
+        User user = userDao.findByUserId(USER_ID);
+        assertThat(user).isEqualTo(new User(USER_ID,"password","name", "email"));
     }
 }
